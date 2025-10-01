e5b6be921aeab1814e8e12ebb80f81dca49f75b1
louispersonal <67544146+louispersonal@users.noreply.github.com>
2025-09-30 20:05:41 -0700

    Added vegetation particle effects

---
+++ Assets/Scripts/DebugView.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DebugView : MonoBehaviour
+{
+    float _timeInWindow = 0f;
+    float _windowLength = 0.5f;
+    int _framesInWindow = 0;
+
+    [SerializeField] TextMeshProUGUI _fpsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_timeInWindow < _windowLength)
+        {
+            _timeInWindow += Time.deltaTime;
+            _framesInWindow++;
+        }
+        else
+        {
+            float frameRate = _framesInWindow / _timeInWindow;
+            string format = string.Format("{0:F2} FPS", frameRate);
+            _fpsText.text = format;
+            _timeInWindow = 0f;
+            _framesInWindow = 0;
+        }
+    }
+}
--- Assets/Scripts/Hex/HexView.cs
+++ Assets/Scripts/Hex/HexView.cs
@@ -6,11 +6,16 @@ public class HexView : MonoBehaviour
 {
 	private BaseHex _data;
 
+	[SerializeField] ParticleSystem _lowVegetationParticles;
+	[SerializeField] ParticleSystem _highVegetationParticles;
+
 	public void Initialize(BaseHex data)
 	{
 		_data = data;
 		gameObject.transform.position = BaseHexGrid.Instance.AxialToSceneConversion(data.Coord);
-	}
+		_lowVegetationParticles.Play();
+		_lowVegetationParticles.Emit(20);
+    }
 
 	public void Terminate()
 	{
