f382ab0d5f6b9ad68c28d02e55f5b713e133a9e3
louispersonal <67544146+louispersonal@users.noreply.github.com>
2025-10-11 15:31:10 -0700

    Dialled in world gen parameters

--- Assets/Scripts/WorldGen/FractalBrownianMotion.cs
+++ Assets/Scripts/WorldGen/FractalBrownianMotion.cs
@@ -6,12 +6,14 @@ using UnityEngine.UIElements;
 
 public class FractalBrownianMotion : MonoBehaviour
 {
-    [SerializeField] int _octaves;
-    [SerializeField] float _lacunarity;
-    [SerializeField] float _gain;
-    [SerializeField] float _seed;
-    [SerializeField] float _baseFrequency;
-    [SerializeField] float _seaLevel;
+    [Range(3, 10)] [SerializeField] int _octaves;
+    [Range(1f, 2f)] [SerializeField] float _lacunarity;
+    [Range(0f, 0.999f)] [SerializeField] float _gain;
+    [Range(3, 10)] [SerializeField] float _seed;
+    [Range(0, 1)] [SerializeField] float _seaLevel;
+    [SerializeField] int _worldWidth;
+    [SerializeField] int _worldHeight;
+    [Range(2f, 8f)] [SerializeField] float _archipelagoness;
 
     private const float SEED_Y_OFFSET = 1.37f;
 
@@ -28,20 +30,23 @@ public class FractalBrownianMotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
-        {
-            RecalculateNoise();
-        }
+
+    }
+
+    public void Generate()
+    {
+        RecalculateNoise();
     }
 
     void RecalculateNoise()
     {
         float baseAmplitude = (1 - _gain) / (1 - Mathf.Pow(_gain, _octaves));
+        float baseFrequency = _archipelagoness / _worldWidth;
 
         rend = GetComponent<Renderer>();
 
         // Set up the texture and a Color array to hold pixels during processing.
-        noiseTex = new Texture2D(300, 300);
+        noiseTex = new Texture2D(_worldWidth, _worldHeight);
         pix = new Color[noiseTex.width * noiseTex.height];
         rend.material.mainTexture = noiseTex;
 
@@ -50,7 +55,7 @@ public class FractalBrownianMotion : MonoBehaviour
         {
             for (float x = 0.0F; x < noiseTex.width; x++)
             {
-                float sample = FBM(new Vector2Int((int)x, (int)y), baseAmplitude, _baseFrequency, _octaves, _lacunarity, _gain, _seed);
+                float sample = FBM(new Vector2Int((int)x, (int)y), baseAmplitude, baseFrequency, _octaves, _lacunarity, _gain, _seed);
                 if (sample < _seaLevel) sample = 0f;
                 pix[(int)y * noiseTex.width + (int)x] = new Color(sample, sample, sample);
             }
