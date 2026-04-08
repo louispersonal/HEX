2763e461cda017b39758e5aa6e25b15dace4f7da
louispersonal <67544146+louispersonal@users.noreply.github.com>
2026-04-08 11:31:25 -0700

    RiverGen iteration

--- Assets/Scripts/WorldGen/RiverGen.cs
+++ Assets/Scripts/WorldGen/RiverGen.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class RiverGen
@@ -7,7 +8,7 @@ public class RiverGen
     private const float MINIMUM_ELEVATION_RIVER_SOURCE = 0.5f;
     private const float MINIMUM_PRECIPITATION_RIVER_SOURCE = 0.5f;
     private const int MAXIMUM_RIVER_LENGTH = 100;
-    private const float BASE_CHANCE = 0.2f;
+    private const float BASE_CHANCE = 0.1f;
 
     public static void GenerateRivers(WorldData world)
     {
@@ -18,6 +19,9 @@ public class RiverGen
             {
                 RiverID newID = new RiverID(riverIndex);
                 River newRiver = new River(newID, data.Coord);
+
+                BuildRiver(newRiver, world);
+
                 AddRiverToWorld(newRiver, world);
                 riverIndex++;
             }
@@ -31,12 +35,49 @@ public class RiverGen
             && Random.Range(0f, 1f) < BASE_CHANCE);
     }
 
+    private static void BuildRiver(River newRiver, WorldData world)
+    {
+        int riverLength = 1;
+        AxialCoordinate currentCoord = newRiver.Source;
+        while (riverLength < MAXIMUM_RIVER_LENGTH)
+        {
+            if (CheckAdjacentSea(currentCoord, world)) break;
+
+            // pick most downhill neighbor
+            world.Grid.TryGetHex(currentCoord, out HexData currentHex);
+            HexData downHillNeighbor = currentHex;
+            foreach (HexData neighbor in HexGridGeometry.HexesInRingOfRadiusOfHex(world.Grid, currentHex, 1))
+            {
+                if (neighbor.ExtraData.Elevation < downHillNeighbor.ExtraData.Elevation) downHillNeighbor = neighbor;
+            }
+
+            if (downHillNeighbor == currentHex) break;
+
+            newRiver.Hexes.Add(downHillNeighbor.Coord);
+
+            currentCoord = downHillNeighbor.Coord;
+
+            riverLength++;
+        }
+        newRiver.Mouth = currentCoord;
+    }
+
+    private static bool CheckAdjacentSea(AxialCoordinate coord, WorldData world)
+    {
+        world.Grid.TryGetHex(coord, out HexData currentHex);
+        foreach (HexData neighbor in HexGridGeometry.HexesInRingOfRadiusOfHex(world.Grid, currentHex, 1))
+        {
+            if (neighbor.ExtraData.IsSea) return true;
+        }
+        return false;
+    }
+
     private static void AddRiverToWorld(River newRiver, WorldData world)
     {
        world.Rivers.Add(newRiver.ID, newRiver);
        foreach(AxialCoordinate coord in newRiver.Hexes)
         {
-            world.RiverLookup.Add(coord, newRiver.ID);
+            world.RiverLookup.TryAdd(coord, newRiver.ID);
         }
     }
 }
