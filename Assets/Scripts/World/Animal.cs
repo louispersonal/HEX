22296f85ff50abdb5b5905ef6bb43952b5f23ed0
louispersonal <67544146+louispersonal@users.noreply.github.com>
2026-04-23 16:14:04 -0700

    Added animal details

---
+++ Assets/Scripts/World/Animal.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalGroup
+{
+    public ushort SpeciesId;
+
+    public ushort RegionId;
+    public int Count;
+}
+
+public class SpeciesProfile
+{
+    public ushort SpeciesId;
+
+    public SizeTier Size;
+
+    public FoodType[] Eats;
+    public Biome[] PreferredBiomes;
+}
+
+public enum SizeTier
+{
+    Small = 0,
+    Medium = 1,
+    Large = 2,
+    Huge = 3
+}
+
+public enum FoodType
+{
+    LowVegetation = 0,
+    HighVegetation = 1,
+    Microfauna = 2,
+    Meat = 3,
+    Fish = 4,
+    Carrion = 5
+}
