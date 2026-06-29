633e54608e281c0be5f7ef85bca52540eb3496ee
louispersonal <67544146+louispersonal@users.noreply.github.com>
2026-06-29 14:46:07 -0700

    Minimap work

--- Assets/Scripts/UI/InGameUI/MiniMap.cs
+++ Assets/Scripts/UI/InGameUI/MiniMap.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,18 @@ using UnityEngine.UI;
 public class MiniMap : MonoBehaviour
 {
     [SerializeField] private RawImage _miniMapImage;
+    [SerializeField] private Image _windowImage;
 
+    private bool _initialized = false;
+    
     public void SetTexture(Texture2D texture)
     {
         _miniMapImage.texture = texture;
+        _initialized = true;
+    }
+
+    private void Update()
+    {
+        if (!_initialized) return;
     }
 }
