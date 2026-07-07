0517bfa40390a4caf3b516bbe533a433a15c11d6
louispersonal <67544146+louispersonal@users.noreply.github.com>
2026-07-07 09:42:36 -0700

    Finished hex selection

--- Assets/Scripts/UI/InGameUI/SelectionManager.cs
+++ Assets/Scripts/UI/InGameUI/SelectionManager.cs
@@ -10,14 +10,33 @@ public class SelectionManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            
+            if (TryGetHexSelection(out ISelectable selectionAttempt)) Select(selectionAttempt);
+            else ClearSelection();
         }
     }
 
-    private bool TryGetHexSelection(out HexView hexView)
+    private void Select(ISelectable selectable)
+    {
+        if (_currentSelection == selectable) return;
+
+        _currentSelection?.OnDeselected();
+
+        _currentSelection = selectable;
+        _currentSelection.OnSelected();
+    }
+
+    private void ClearSelection()
+    {
+        _currentSelection?.OnDeselected();
+        _currentSelection = null;
+    }
+    
+    private bool TryGetHexSelection(out ISelectable selection)
     {
         HexGrid grid = GameController.Instance.SessionManager.WorldData.Grid;
         HexData target = HexGridGeometry.GetHexAtScenePoint(grid, HexGridView.MouseToPlane(Camera.main, 0f));
-        return GameSceneController.Instance.HexGridView.TryGetLiveHex(target.Coord, out hexView);
+        bool success =  GameSceneController.Instance.HexGridView.TryGetLiveHex(target.Coord, out HexView hexView);
+        selection = success ? hexView : null;
+        return success;
     }
 }
